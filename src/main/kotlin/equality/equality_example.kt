099955b6a284e6e8495b9921619b5e5ca1722598
Imnmania <niloyb@live.com>
2022-08-18 21:32:18 +0600

    equality

---
+++ src/main/kotlin/equality/equality_example.kt
@@ -0,0 +1,23 @@
+fun main() {
+    // structural
+    var name1 = "Donn"
+    var name2 = "Felker"
+
+    println(name1 == name2)
+    println(name1.equals(name2)) // similar to
+
+    println(name1 != name2)
+
+    // referential
+    var a = 12
+    var b = 12
+
+    println(a === b) // doesn't work with primitives
+
+    var c = Person("Donn")
+    var d = Person("Donn")
+    println(c !== d)
+
+}
+
+class Person(var name: String)
