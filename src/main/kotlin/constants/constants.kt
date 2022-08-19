e022e30bcb0bf948e9b483cd7676dc2d5d0b19bc
Imnmania <niloyb@live.com>
2022-08-19 20:36:38 +0600

    constants

---
+++ src/main/kotlin/constants/constants.kt
@@ -0,0 +1,4 @@
+object Constants {
+    const val MAX_AGE = 19
+    const val MAX_HEIGHT = 6
+}
---
+++ src/main/kotlin/constants/constants_example.kt
@@ -0,0 +1,25 @@
+package constants
+
+import Constants
+
+fun main() {
+    var A = User("Jon", "Doe")
+    println(A.MAX_AGE)
+    println(User.MAX_HEIGHT)
+
+    println(Constants.MAX_AGE)
+    println(Constants.MAX_HEIGHT)
+}
+
+class User(var firstName: String, var lastName: String) {
+
+    val MAX_AGE = 20
+
+    companion object {
+        const val MAX_HEIGHT = 6
+    }
+
+    override fun toString(): String {
+        return "User ( First Name => $firstName, Last Name => $lastName )"
+    }
+}
