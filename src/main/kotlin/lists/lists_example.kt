9b79c5f07c59451e36ae3916a44c1e8fb012ca5d
Imnmania <niloyb@live.com>
2022-08-24 23:08:18 +0600

    immutable list in kotlin

---
+++ src/main/kotlin/lists/lists_example.kt
@@ -0,0 +1,15 @@
+fun main() {
+    // * immutable list
+    val items = listOf(1, 2, 3, 4, 5)
+    items.forEach( { println(it) } )
+
+    val users = listOf<User>(
+        User("Niloy", "Biswas"),
+        User(firstName = "John", lastName = "Doe")
+    )
+    users.forEach( { println(it) } )
+
+    val name: String? = "null"
+    val stuff = if (name == null) emptyList<String>() else listOf<String>(name)
+    println(stuff)
+}
