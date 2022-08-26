8d33ae1cfcce7bbcbed9902bc5d6c1cf4778dcd0
Imnmania <niloyb@live.com>
2022-08-27 01:04:26 +0600

    sequence generate in kotlin

---
+++ src/main/kotlin/sequence_function/sequence_example.kt
@@ -0,0 +1,16 @@
+import java.util.concurrent.TimeUnit
+import kotlin.system.measureNanoTime
+
+fun main() {
+
+    val list = generateSequence(1) { it + 1 }
+        .take(50_000_000)
+        .toList()
+
+    val result = list.filter { it % 3 == 0 }.average()
+
+    print(list)
+    println()
+    println(result)
+    println("Done!")
+}
