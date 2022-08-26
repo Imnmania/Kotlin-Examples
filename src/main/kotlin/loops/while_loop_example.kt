63686d5416b02a60d6da71c6f99746b165e7aac7
Imnmania <niloyb@live.com>
2022-08-26 20:34:16 +0600

    while loops in kotlin

---
+++ src/main/kotlin/loops/while_loop_example.kt
@@ -0,0 +1,15 @@
+fun main() {
+    var x = 0
+    while (x < 10) {
+        println(x)
+        x++
+    }
+    println("All done")
+}
+
+fun keepGoing(): Boolean {
+    // checking file system
+    // checking api
+
+    return true
+}
