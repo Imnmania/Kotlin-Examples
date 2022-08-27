38e8468cf6a871ff6c020026ee8bfe3937349640
Imnmania <niloyb@live.com>
2022-08-27 18:23:34 +0600

    differences between collection types

---
+++ src/main/kotlin/difference_in_collection_types/example_1.kt
@@ -0,0 +1,15 @@
+fun main() {
+
+    // List
+    val list = listOf("Donn", "Niloy", "Felker", "Biswas")
+
+    // Set
+    val set = setOf("Donn", "Niloy", "Felker", "Biswas", "Donn")
+
+    // Map: Key value pairs
+    val map = mapOf("Donn" to "Felker", "Niloy" to "Biswas", "Evelyn" to "Smith")
+
+    // Sequence: performance
+    val sequence = sequenceOf(1, 2, 3, 4, 5)
+
+}
