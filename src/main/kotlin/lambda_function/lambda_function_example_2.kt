4d11825d8c21fab55b47f10f8f0610b46211f2bb
Imnmania <niloyb@live.com>
2022-08-31 20:01:12 +0600

    passing lambda expressions as function parameters

--- src/main/kotlin/lambda_function/lambda_function_example_2.kt
+++ src/main/kotlin/lambda_function/lambda_function_example_2.kt
@@ -1,11 +1,29 @@
 package lambda_function
 
+/*
+*  val lambdaName: (InputType) -> ReturnType = { arguments: InputType -> body }
+* */
+
 fun main() {
 
     lineLogger {
         println("Message 1")
         println("Message 2")
         println("Message 3")
+    }
+
+    repeater(5) {index ->
+        if (index < 3) {
+            // 3 times
+            println("Hello")
+        } else {
+            // 2 times
+            println("Goodbye")
+        }
+    }
+
+    derbyAnnouncer { player: String ->
+        "$player is a great asset to the team!"
     }
 
 }
@@ -16,4 +34,24 @@ fun lineLogger(block: () -> Unit) {
     repeat(2) { println("-----------") }
     block()
     repeat(2) { println("-----------") }
+}
+
+fun repeater(times: Int, block: (Int) -> Unit) {
+    repeat(times) { index -> block(index) }
+}
+
+fun derbyAnnouncer(block: (String) -> String) {
+    val players = listOf(
+        "McGwire",
+        "Canseco",
+        "HoneyCutt",
+        "Davis",
+        "Dawley",
+        "Weiss"
+    )
+
+    val randomPlayer = players.random()
+    println("The next player is ... $randomPlayer")
+    val announcerMessage = block(randomPlayer)
+    println(announcerMessage)
 }
